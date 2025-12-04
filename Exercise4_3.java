576f7fee9422a1da701ac94749cd0107c1c041c7
mehmet <140273584+mehmet-furkan@users.noreply.github.com>
2025-12-04 20:41:15 +0300

    Enhance Exercise4_3.java with comments and documentation
    Added comments and documentation for clarity.

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,3 +1,9 @@
+/*
+ * Ad Soyad: Mehmet Furkan AKYAR
+ * Ogrenci No: 240541056
+ * Tarih: 12/4/2025
+ */
+
 public class Exercise4_3 {
 
     public static void zoop() {
@@ -22,3 +28,19 @@ public class Exercise4_3 {
         System.out.println(".");
     }
 }
+// Kodun çalışma sırası:
+// 1.main başlar.
+// 2.main içinde zoop() çağrılır.
+// 3.zoop içinde ilk satırda baffle() çağrılır.
+// 4.baffle içinde ping() çağrılır (Bu, ping'in ilk çağrılışıdır).
+
+/* Stack (Yığın),Açıklama
+ * ping,"En üstte, şu an çalışan metot."
+ * baffle,ping metodunu çağıran metot.
+ * zoop,baffle metodunu çağıran metot.
+ * main,"Programın başlangıç noktası, zoop metodunu çağıran metot." */
+
+/* Programın Tam Çıktısı
+No, I wug.
+You wugga wug.
+I wug. */
